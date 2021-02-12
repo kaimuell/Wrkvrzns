e35e205d8d0f8b16cdebe48522882dc21d7e7c08
TheArtistGuy <info@kaimueller-art.de>
2021-02-12 22:17:42 +0100

    added javadoc

--- src/gui/elements/FilterType.java
+++ src/gui/elements/FilterType.java
@@ -1,5 +1,8 @@
 package gui.elements;
 
+/**
+ * Enum um eine Auswahl zu treffen nach welchem Parameter eines {@link model.elements.ArtPieceEntry} sortiert werden soll
+ */
 public enum FilterType {
     NAME, TECHNIQUE, YEAR, TYPE
 }
