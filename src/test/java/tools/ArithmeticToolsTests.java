2e6a930c1409ac99b8aaaefb1ae52cba0065e6ad
TheArtistGuy <info@kaimueller-art.de>
2021-09-24 21:09:02 +0200

    added adjust Price Of Selected Entries, Bugfix in ArithmeticTools

---
+++ src/test/java/tools/ArithmeticToolsTests.java
@@ -0,0 +1,22 @@
+package tools;
+
+import org.junit.Test;
+
+import static tools.ArithmeticTools.*;
+
+public class ArithmeticToolsTests {
+
+    @Test
+    void testArithmeticTools(){
+        assert (roundToTen(15) == 20);
+        assert (roundToTen(12) == 10);
+        assert (roundToHundred(162) == 200);
+        assert (roundToHundred(222) == 200);
+        assert (roundToHundred(0) == 0);
+        assert (roundToFifty(1526) == 1500);
+        assert (roundToFifty(1042) == 1050);
+        assert (roundToFifty(1182) == 1200);
+        assert (addPercentage(100, 10) == 110.0);
+        assert (subtractPercentage(100, 20) == 80);
+    }
+}
